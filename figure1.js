706700a759bfcf56e1679f9f7e556238aed2675a
Mingrui Yin <mingrui4@illinois.edu>
2019-04-26 23:00:34 -0500

    change vega

--- figure1.js
+++ figure1.js
@@ -2,92 +2,117 @@ Reveal.addEventListener('ready', function(){
 var yourVlSpec = {
     "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
     "data": {
-        "url": "https://raw.githubusercontent.com/mingrui4/spr2019-adv-project/master/output.csv"
+      "url": "https://raw.githubusercontent.com/mingrui4/spr2019-adv-project/master/output.csv"
     },
-    "title": {
-        "text": "2014-2015 Daily Temperature (F) in CLT",
-        "anchor": "middle"
-    },
-    "config": {
-        "axis": {
-            "domain": false
-        }
-    },
-    "vconcat": [
-        {
-            "width": 960,
-            "height": 400,
-            "mark": "rect",
-            "encoding": {
-                "x": {
-                    "field": "date",
-                    "timeUnit": "date",
-                    "type": "ordinal",
-                    "title": "Day",
-                    "axis": {
-                        "labelAngle": 0,
-                        "format": "%e"
-                    }
-                },
-                "y": {
-                    "scale": {
-                        "domain": {
-                            "selection": "brush"
-                        }
-                    },
-                    "field": "date",
-                    "timeUnit": "month",
-                    "type": "ordinal",
-                    "title": "Month"
-                },
-                "color": {
-                    "field": "KCLT_temp",
-                    "aggregate": "max",
-                    "type": "quantitative",
-                    "legend": {
-                        "title": null
-                    }
-                }
-            }
+    "config": {"axis": {"domain": false}},
+    "columns": 2,
+    "concat": [
+      {
+        "title": {
+          "text": "2014-2015 Daily Temperature (F) in SEA",
+          "anchor": "middle"
         },
-        {
-            "width": 960,
-            "height": 250,
-            "mark": {
-                "type": "line",
-                "point": {
-                    "filled": false,
-                    "fill": "grey"
-                }
-            },
-            "selection": {
-                "brush": {
-                    "type": "interval",
-                    "encodings": [
-                        "x"
-                    ]
-                }
-            },
-            "encoding": {
-                "x": {
-                    "timeUnit": "month",
-                    "field": "date",
-                    "type": "ordinal",
-                    "title": "Date"
-                },
-                "y": {
-                    "aggregate": "mean",
-                    "field": "KCLT_temp",
-                    "type": "quantitative",
-                    "title": "Temp"
-                },
-                "color": {
-                    "value": "grey"
-                }
-            }
+        "width": 960,
+        "height": 400,
+        "mark": "rect",
+        "encoding": {
+          "x": {
+            "field": "date",
+            "timeUnit": "date",
+            "type": "ordinal",
+            "title": "Day",
+            "axis": {"labelAngle": 0, "format": "%e"}
+          },
+          "y": {
+            "scale": {"domain": {"selection": "brush"}},
+            "field": "date",
+            "timeUnit": "month",
+            "type": "ordinal",
+            "title": "Month"
+          },
+          "color": {
+            "field": "KSEA_temp",
+            "aggregate": "max",
+            "type": "quantitative",
+            "legend": {"title": null}
+          }
         }
+      },
+      {
+        "title": {
+          "text": "2014-2015 Daily Temperature (F) in nyc",
+          "anchor": "middle"
+        },
+        "width": 960,
+        "height": 400,
+        "mark": "rect",
+        "encoding": {
+          "x": {
+            "field": "date",
+            "timeUnit": "date",
+            "type": "ordinal",
+            "title": "Day",
+            "axis": {"labelAngle": 0, "format": "%e"}
+          },
+          "y": {
+            "scale": {"domain": {"selection": "brush"}},
+            "field": "date",
+            "timeUnit": "month",
+            "type": "ordinal",
+            "title": "Month"
+          },
+          "color": {
+            "field": "KSEA_temp",
+            "aggregate": "max",
+            "type": "quantitative",
+            "legend": {"title": null}
+          }
+        }
+      },
+      {
+        "width": 960,
+        "height": 250,
+        "mark": {"type": "line", "point": {"filled": false, "fill": "#60acfc"}},
+        "selection": {"brush": {"type": "interval", "encodings": ["x"]}},
+        "encoding": {
+          "x": {
+            "timeUnit": "month",
+            "field": "date",
+            "type": "ordinal",
+            "title": "Date"
+          },
+          "y": {
+            "aggregate": "mean",
+            "field": "KNYC_temp",
+            "type": "quantitative",
+            "title": "Temp"
+          },
+          "color": {"value": "#60acfc"}
+        }
+      },
+      {
+        "width": 960,
+        "height": 250,
+        "mark": {"type": "line", "point": {"filled": false, "fill": "#ff9a07"}},
+        "selection": {"brush": {"type": "interval", "encodings": ["x"]}},
+        "encoding": {
+          "x": {
+            "timeUnit": "month",
+            "field": "date",
+            "type": "ordinal",
+            "title": "Date"
+          },
+          "y": {
+            "aggregate": "mean",
+            "field": "KNYC_temp",
+            "type": "quantitative",
+            "title": "Temp"
+          },
+          "color": {"value": "#ff9a07"}
+        }
+      }
     ]
-};
+  };
 
 var embedded = vegaEmbed('#vis', yourVlSpec);
 });
